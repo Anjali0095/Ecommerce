9704c5a25a2e310efd7c0001adf8479b38c91bdb
Anjali <smoni6465@gmail.com>
2024-04-23 19:11:43 +0530

    changes done

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,21 +1,17 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-        height: 30px;
-        background-color: teal;
-        color: white;
-        text-align:center;
-        justify-content: center;
-        display: flex;
-        font-size: 14px;
-        font-weight:500;
-        `
+  height: 30px;
+  background-color: teal;
+  color: white;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  font-size: 14px;
+  font-weight: 500;
+`;
 const Announcement = () => {
-    return (
-        <Container>
-            Super Deal! Free Shipping on Orders over $100
-        </Container>
-    )
-}
+  return <Container>Super Deal! Free Shipping on Orders over $50</Container>;
+};
 
-export default Announcement
+export default Announcement;
